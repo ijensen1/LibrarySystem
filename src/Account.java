f27bc788ed6e7ef4c9cf9ff279e15c33b39ee3cb
ijensen <ijensen@techvalleyhigh.org>
2019-10-30 08:09:35 -0400

    updated Account based Classes

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,42 @@
 public class Account {
 
-    private String name;
+    private String firstName;
+    private String lastName;
 
+
+    public Account(){
+
+        this("John", "Smith");
+
+    }
+
+    public Account(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName(){
+
+        return getFirstName();
+
+    }
+
+    public String getLastName(){
+
+        return getLastName();
+
+    }
+
+    public void setFirstName(){
+
+        this.firstName = firstName;
+
+    }
+
+    public void setLastName(){
+
+        this.lastName = lastName;
+
+    }
 
 }
--- src/AccountCurrentBorrowStatus.java
+++ src/AccountCurrentBorrowStatus.java
@@ -1,13 +1,41 @@
 public class AccountCurrentBorrowStatus {
 
     private String barrowStatus;
+    private String barrowBranch;
 
     public AccountCurrentBorrowStatus(){
-        this("in")
+
+        this("in", "National Branch");
     }
 
-    public AccountCurrentBorrowStatus(String barrowStatus){
+    public AccountCurrentBorrowStatus(String barrowStatus, String barrowBranch){
         this.barrowStatus = barrowStatus;
+        this.barrowBranch = barrowBranch;
+    }
+
+    public String getBarrowStatus(){
+
+        return barrowStatus;
+
+    }
+
+    public String getBarrowBranch(){
+
+        return barrowBranch;
+
+    }
+
+
+    public void setBarrowStatus(){
+
+        this.barrowStatus = barrowStatus;
+
+    }
+
+    public void setBarrowBranch(){
+
+        this.barrowBranch = barrowBranch;
+
     }
 
 }
