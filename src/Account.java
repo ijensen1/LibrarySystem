193898c1f1674f00f794bf2c9f4441c7407a5733
ijensen <ijensen@techvalleyhigh.org>
2019-10-31 08:53:36 -0400

    took out sub account classes

--- src/Account.java
+++ src/Account.java
@@ -1,32 +1,60 @@
+import java.io.ObjectInputStream;
+
 public class Account {
 
+    //setting Class data fields
     private String firstName;
     private String lastName;
-
+    private String barrowStatus;
+    private String barrowBranch;
+    private String phoneNumber;
+    private String email;
+    private String address;
 
     public Account(){
 
-        this("John", "Smith");
+        this("John", "Smith", "in", "National Branch", "(000) 000-0000", "jsmith@email.com", "3417 State Route 30 Fultonham NY 12071");
+
 
     }
 
-    public Account(String firstName, String lastName){
+    public Account(String firstName, String lastName, String barrowStatus, String barrowBranch, String phoneNumber, String email, String address){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.barrowStatus = barrowStatus;
+        this.barrowBranch = barrowBranch;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
     }
 
     public String getFirstName(){
-
         return getFirstName();
-
     }
 
     public String getLastName(){
-
         return getLastName();
-
     }
 
+    public String getBarrowStatus(){
+        return barrowStatus;
+    }
+
+    public String getBarrowBranch(){
+        return barrowBranch;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getAddress(){
+        return address;
+    }
     public void setFirstName(){
 
         this.firstName = firstName;
