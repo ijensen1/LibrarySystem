f72f349f9f466ec2432a6d8f921267db0163926c
LEGOJACKI <jack.gs.code@gmail.com>
2019-11-14 11:03:45 -0500

    Added more functionality to frontend, fixed LibraryManager

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 
 public class FrontEnd {
   
   
   public static void main(String[] args) { 
     LibraryManager master = new LibraryManager();
-    System.out.println(master.getAccount("John Smith").getAddress());
+    Scanner input = new Scanner(System.in);
+    System.out.println("What would you like to do?");
+    System.out.print("Options are: testAccount, testAcntAdd");
+    String cmd = input.nextLine();
+    
+    if (cmd.equals("testAccount")) {
+      System.out.println(master.getAccount("John Smith").getAddress());
+    }
+    
+    if (cmd.equals("testAcntAdd")) {
+      System.out.println(master.getAccountsLength());
+      master.addAccount(master.getAccount("John Smith"));
+      System.out.println(master.getAccountsLength());
+    }
   }
   
 }
