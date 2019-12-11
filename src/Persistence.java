f6013e2712878c27d472360aaf178b7df8cb76b6
AlfredBurgazoliii <aburgazoli@techvalleyhigh.org>
2019-12-11 08:48:50 -0500

    Changed how the persistence class works, just takes in where in the data folder you want to save/load to/from now instead of the File to save/load to/from.

--- src/Persistence.java
+++ src/Persistence.java
@@ -10,9 +10,14 @@ class Persistence {
     static final String splitter = "::"; //used between tags for when reading from or saving to a file
     static final String dataPath = "../Data/"; //where is data in general being stored
 
-    static void saveToFile(File saveTo, Borrowable[] data) {
+    /**
+     * Method to save an array of Borrowables to a given File.
+     * @param savePath where the file to save to is located.
+     * @param data the Borrowables to save.
+     */
+    static void saveToFile(String savePath, Borrowable[] data) {
         try {
-            PrintWriter saveFile = new PrintWriter(saveTo); //To write data to file
+            PrintWriter saveFile = new PrintWriter(dataPath + savePath); //To write data to file
             for (Borrowable borrowable : data) {
                 saveFile.println(borrowable.makeString()); //Convert each borrowable into a string and print to file
             }
@@ -23,9 +28,14 @@ class Persistence {
         }
     }
 
-    static void saveToFile(File saveTo, Account[] data) {
+    /**
+     * Method to save an array of Accounts to a given File.
+     * @param savePath where the file to save to is located.
+     * @param data the Accounts to save.
+     */
+    static void saveToFile(String savePath, Account[] data) {
         try {
-            PrintWriter saveFile = new PrintWriter(saveTo); //To write data to file
+            PrintWriter saveFile = new PrintWriter(dataPath + savePath); //To write data to file
             for (Account account : data) {
                 saveFile.println(account.makeString()); //Convert each account into a string and print to file
             }
@@ -36,12 +46,25 @@ class Persistence {
         }
     }
 
-    static Borrowable[] loadBorrowables(File loadFrom) {
+    /**
+     * Method to load an array of Borrowables from a given File.
+     * @param loadPath where the file to load from is located.
+     * @return
+     */
+    static Borrowable[] loadBorrowables(String loadPath) {
         try {
-            Scanner readFile = new Scanner(loadFrom); //To read from file
             Borrowable[] data = new Borrowable[0]; //To hold data that the file contains
+            File load = new File(dataPath + loadPath); //To hold the file
+
+            //Check if the file exists
+            if (!load.exists())
+                return data; //Return a length 0 array if the file doesn't exist
+
+            Scanner readFile = new Scanner(load); //To read from file
             String[] current; //To hold the current Borrowable being read
-            while (readFile.hasNextLine()) { //Go through every Borrowable in the file
+
+            //Load all lines from the Scanner
+            while (readFile.hasNextLine()) {
                 data = Arrays.copyOf(data, data.length + 1); //Extend data by one
                 current = readFile.nextLine().split(Persistence.splitter); //Read the next Borrowable and split it into its tags
                 data[data.length - 1] = new Borrowable(current[0], current[1], current[2], current[3]); //Put the current Borrowable in data
@@ -54,17 +77,25 @@ class Persistence {
         }
     }
 
-    static Account[] loadAccounts(File loadFrom) {
+    static Account[] loadAccounts(String loadPath) {
         try {
-            Scanner readFile = new Scanner(loadFrom); //To read from file
             Account[] data = new Account[0]; //To hold data that the file contains
-            String[] current; //To hold the current Account being read
-            while (readFile.hasNextLine()) { //Go through every Account in the file
+            File load = new File(dataPath + loadPath); //To hold the file
+
+            //Check if the file exists
+            if (!load.exists())
+                return data; //Return a length 0 array if the file doesn't exist
+
+            Scanner readFile = new Scanner(load); //To read from file
+            String[] current; //To hold the current Borrowable being read
+
+            //Load all lines from the Scanner
+            while (readFile.hasNextLine()) {
                 data = Arrays.copyOf(data, data.length + 1); //Extend data by one
-                current = readFile.nextLine().split(Persistence.splitter); //Read the next Account and split it into its tags
-                data[data.length - 1] = new Account(current[0], current[1], current[2], current[3]); //Put the current Account in data
+                current = readFile.nextLine().split(Persistence.splitter); //Read the next Borrowable and split it into its tags
+                data[data.length - 1] = new Account(current[0], current[1], current[2], current[3]); //Put the current Borrowable in data
             }
-            return data;//Return data
+            return data; //Return data
         } catch (IOException e) {
             //Some error occurred while trying to load from the file
             System.out.println("Error trying to load file.");
