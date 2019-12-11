3b985203627b706bdb9ac4cb29ab3770774e3009
Alfred <aburgazo@gmail.com>
2019-12-10 20:10:21 -0500

    Created Borrowable class.

---
+++ src/Borrowable.java
@@ -0,0 +1,77 @@
+/**
+ * To create a single book/dvd/cd.
+ */
+class Borrowable {
+    private String title, //To hold the work's title
+            creator, //To hold the work's creator
+            genre1, //To hold the work's first genre
+            genre2, //To hold the work's second genre
+            inOut; //To hold whether or not the work is checked in or out
+
+    /**
+     * Constructor to take in and set the tags to appropriate values.
+     * @param title title of the work.
+     * @param creator creator of the work.
+     * @param genre1 first genre of the work.
+     * @param genre2 second genre of the work.
+     */
+    Borrowable(String title, String creator, String genre1, String genre2) {
+        this.title = title;
+        this.creator = creator;
+        this.genre1 = genre1;
+        this.genre2 = genre2;
+        inOut = "in";
+    }
+
+    /**
+     * Method to convert the Borrowable's tags into a single String.
+     * @return a String of the Borrowable's tags.
+     */
+    String makeString(){
+        return (title + Persistence.splitter +
+                creator + Persistence.splitter +
+                genre1 + Persistence.splitter +
+                genre2 + Persistence.splitter +
+                inOut);
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    void setTitle(String title) {
+        this.title = title;
+    }
+
+    String getCreator() {
+        return creator;
+    }
+
+    void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    String getGenre1() {
+        return genre1;
+    }
+
+    void setGenre1(String genre1) {
+        this.genre1 = genre1;
+    }
+
+    String getGenre2() {
+        return genre2;
+    }
+
+    void setGenre2(String genre2) {
+        this.genre2 = genre2;
+    }
+
+    String getInOut() {
+        return inOut;
+    }
+
+    void setInOut(String inOut) {
+        this.inOut = inOut;
+    }
+}
