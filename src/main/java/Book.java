b48983e1fa9b2b6c379b2c134c7092b5246c995d
AlfredBurgazoliii <aburgazoli@techvalleyhigh.org>
2020-02-27 08:58:47 -0500

    Finished up constructors for Book, CD, and DVD. Also made getters and setters for said classes.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,7 +1,47 @@
+/**
+ * Class to hold information about a book.
+ */
 public class Book extends Borrowable{
-    private String author;
+    private String author, isbn;
+    private boolean hardcover;
 
+    /**
+     * Constructor to make a Book object.
+     * @param home the home library of the book.
+     * @param title the title of the book.
+     * @param genres the genres of the book.
+     * @param author the author of the book.
+     * @param isbn the ISBN number of the book.
+     * @param hardcover whether or not the book is hardcover.
+     */
     Book(String home, String title, String[] genres, String author, String isbn, boolean hardcover) {
         super(home, title, genres);
+        this.author = author;
+        this.isbn = isbn;
+        this.hardcover = hardcover;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isHardcover() {
+        return hardcover;
+    }
+
+    public void setHardcover(boolean hardcover) {
+        this.hardcover = hardcover;
     }
 }
