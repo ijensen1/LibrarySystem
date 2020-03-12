95af19f38e6de8dbff8c7b2133312c5e30d20407
AlfredBurgazoliii <aburgazoli@techvalleyhigh.org>
2020-03-12 09:16:58 -0400

    Added a isPopUpBook variable to PictureBook.

--- src/main/java/PictureBook.java
+++ src/main/java/PictureBook.java
@@ -3,6 +3,7 @@
  */
 public class PictureBook extends Book {
     protected String illustrator;
+    protected boolean isPopupBook;
 
     /**
      * Constructor to make a Book object.
@@ -13,10 +14,12 @@ public class PictureBook extends Book {
      * @param isbn the ISBN number of the book.
      * @param hardcover whether or not the book is hardcover.
      * @param illustrator the picture book's illustrator.
+     * @param isPopupBook whether or not the picture book is also a pop-up book.
      */
-    PictureBook(String home, String title, String[] genres, String author, String isbn, boolean hardcover, String illustrator) {
+    PictureBook(String home, String title, String[] genres, String author, String isbn, boolean hardcover, String illustrator, boolean isPopupBook) {
         super(home, title, genres, author, isbn, hardcover);
         this.illustrator = illustrator;
+        this.isPopupBook = isPopupBook;
     }
 
     public String getIllustrator() {
@@ -25,6 +28,14 @@ public class PictureBook extends Book {
 
     public void setIllustrator(String illustrator) {
         this.illustrator = illustrator;
+    }
+
+    public boolean isPopupBook() {
+        return isPopupBook;
+    }
+
+    public void setPopupBook(boolean popupBook) {
+        isPopupBook = popupBook;
     }
 
     public String[] getPeople(){
